5ac547abe587974dbded6eaeb6bbff67c554f95b
Vishal Myla <99010982+VishalMyla@users.noreply.github.com>
2023-08-31 18:08:35 +0530

    Async Error handling is being done

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const {notFound , errorHandler} = require('./middleware/errorHandler';)
 require('dotenv').config();
 const app = express();
 const dbConnect = require("./config/dbConnect")
-const authrouter = require('./routes/authrouter');
+// const authrouter = require('./routes/user_routes');
+const router = require('./routes/conjunction')
 const port = process.env.PORT ;
 console.log(port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 dbConnect();
-// Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, World from Express!');
-});
-
-app.use('/api/user', authrouter);
-
+app.use('/',router);
+app.use(notFound);
+app.use(errorHandler);
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
